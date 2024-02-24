ebec1c3e0da98b3a588bb9531f1c47c2c2cba76c
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-24 11:28:09 +0000

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main>
+      <div className="about-me">
+        <h1>Nandini Polavarapu</h1>
+        <p> Software Developer</p>
+      </div>
+      <div className="my-projects"></div>
+    </main>
+  )
+}
+
+export default App

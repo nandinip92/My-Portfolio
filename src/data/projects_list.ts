225720118040c258d0a02c0ecc38089049598f29
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-03-10 22:20:05 +0000

    styles: Adding new styles ad backgrounds

--- src/data/projects_list.ts
+++ src/data/projects_list.ts
@@ -8,29 +8,16 @@ export const projects: Project[] = [
     link: "https://nandinip92.github.io/photo-viewer-react-app/",
   },
   {
-    name: "MyFace",
-    description: "A social media platform",
-    image: "",
-    link: "https://github.com/nandinip92/myface-api-typescript/tree/main/frontend",
-  },
-  {
     name: "Pokemon Encyclopedia",
     description: "A fun way of discovering pokemon",
-    image: "",
-    link: "https://github.com/nandinip92/pokemon-app",
-  },
-  {
-    name: "Mars Mission",
-    description:
-      "A site to encourage young people to find out more about space",
-    image: "",
-    link: "#",
+    image: "/PokemonApp/pokemon-app.PNG",
+    link: "https://nandinip92.github.io/pokemon-app/",
   },
   {
     name: "Cat Cards",
     description:
       "A site to encourage young people to find out more about space",
-    image: "",
+    image: "/CatCards/cat-cards.PNG",
     link: "https://nandinip92.github.io/lm-code-react-props-state-cat-cards/",
   },
   {
@@ -39,5 +26,19 @@ export const projects: Project[] = [
       "A site to encourage young people to find out more about space",
     image: "",
     link: "https://nandinip92.github.io/lm-code-intro-react-router-fakelandia/",
+  },
+  {
+    name: "MyFace",
+    description: "A social media platform",
+    image: "",
+    link: "https://github.com/nandinip92/myface-api-typescript/tree/main/frontend",
+  },
+
+  {
+    name: "Mars Mission",
+    description:
+      "A site to encourage young people to find out more about space",
+    image: "",
+    link: "#",
   },
 ]

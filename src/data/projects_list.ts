203630afd0a5f07c1dc7790ff440576ec798598a
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-05-02 10:47:47 +0100

    new projects: adding new projects

--- src/data/projects_list.ts
+++ src/data/projects_list.ts
@@ -2,6 +2,41 @@ import { Project } from "../models/Project"
 //image: "./assets/PhotoViewer/photo-viewer-app-sceenshot.png",
 export const projects: Project[] = [
   {
+    name: "Zoo Management",
+    description:
+      "A site to encourage young people to find out more about space",
+    image: "./assets/ZooManagemet/ZooAnimals.png",
+    link: "https://github.com/nandinip92/ZooManagement.git",
+  },
+
+  {
+    name: "Mars Mission",
+    description:
+      "Group project to create a site to encourage young people to find out more about space",
+    image: "./assets/MarsMission/mars-mission.png",
+    link: "https://nandinip92.github.io/mars-mission/",
+  },
+  {
+    name: "MyFace",
+    description: "A social media platform with user profiles, posts with likes and dislikes",
+    image: "./assets/MyFace/my-face.png",
+    link: "https://github.com/nandinip92/myface-api-typescript/tree/main/frontend",
+  },
+  {
+    name: "Whale Spotting",
+    description:
+      "Group project help users find out more about whales and to encourage them to go and spot whales in the wild",
+    image: "./assets/WhaleSpotting/whales.png",
+    link: "https://github.com/techswitch-learners/whale-spotting-spring-2024.git",
+  },
+  {
+    name: "FakeLandia",
+    description:
+      "A site to encourage young people to find out more about space",
+    image: "./assets/Fakelandia/FakeLandia.PNG",
+    link: "https://nandinip92.github.io/lm-code-intro-react-router-fakelandia/",
+  },
+  {
     name: "Photo Viewer",
     description: "A simple React photo application",
     image: "./assets/PhotoViewer/photo-viewer.png",
@@ -18,26 +53,5 @@ export const projects: Project[] = [
     description: "Simple React app for creating Cat/Dog cards using the form",
     image: "./assets/CatCards/cat-cards.PNG",
     link: "https://nandinip92.github.io/lm-code-react-props-state-cat-cards/",
-  },
-  {
-    name: "FakeLandia",
-    description:
-      "A site to encourage young people to find out more about space",
-    image: "./assets/Fakelandia/FakeLandia.PNG",
-    link: "https://nandinip92.github.io/lm-code-intro-react-router-fakelandia/",
-  },
-  {
-    name: "MyFace",
-    description: "A social media platform",
-    image: "./assets/MyFace/my-face.png",
-    link: "https://github.com/nandinip92/myface-api-typescript/tree/main/frontend",
-  },
-
-  {
-    name: "Mars Mission",
-    description:
-      "A site to encourage young people to find out more about space",
-    image: "./assets/MarsMission/mars-mission.png",
-    link: "https://nandinip92.github.io/mars-mission/",
   },
 ]
